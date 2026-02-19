afe8e5759abb8e512cb0b4114b9e341ac9cd27e6
dealexis <flytzitv@gmail.com>
2026-02-19 16:29:46 +0100

    fix fail - del

--- components/cards/StuffCard.tsx
+++ components/cards/StuffCard.tsx
@@ -36,25 +36,34 @@ export default function StuffCard({
           </div>
         )}
       </div>
-      {url ? (
-        <Link
-          href={url}
-          target="_blank"
-          className="bg-[#DCDCDC] rounded-lg p-2 w-full flex flex-col items-start gap-2 relative"
-        >
-          <KeyValue label="Nom" value={name} />
-          <KeyValue label="Marque" value={brand} />
-          <KeyValue label="Catégorie" value={category} />
-          <KeyValue label="Poids" value={weight + " g"} />
-        </Link>
-      ) : (
-        <div className="bg-[#DCDCDC] rounded-lg p-2 w-full flex flex-col items-start gap-2 relative">
-          <KeyValue label="Nom" value={name} />
-          <KeyValue label="Marque" value={brand} />
-          <KeyValue label="Catégorie" value={category} />
-          <KeyValue label="Poids" value={weight + " g"} />
+
+      <div className="relative group w-full">
+        <div className="absolute top-2 right-2 z-10 flex flex-row gap-1">
+          <ActionButton
+            item={{ id, name, brand, category, weight, url, image }}
+          />
         </div>
-      )}
+
+        {url ? (
+          <Link
+            href={url}
+            target="_blank"
+            className="bg-[#DCDCDC] rounded-lg p-2 w-full flex flex-col items-start gap-2 relative"
+          >
+            <KeyValue label="Nom" value={name} />
+            <KeyValue label="Marque" value={brand} />
+            <KeyValue label="Catégorie" value={category} />
+            <KeyValue label="Poids" value={weight + " g"} />
+          </Link>
+        ) : (
+          <div className="bg-[#DCDCDC] rounded-lg p-2 w-full flex flex-col items-start gap-2 relative">
+            <KeyValue label="Nom" value={name} />
+            <KeyValue label="Marque" value={brand} />
+            <KeyValue label="Catégorie" value={category} />
+            <KeyValue label="Poids" value={weight + " g"} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
