3c4eaa3c7a88c9a7d64b32b8bc8351b392eeedc9
dealexis <flytzitv@gmail.com>
2026-07-31 14:07:49 +0200

    chore: initialize project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added logo.svg for branding.
    - Initialized TypeScript configuration in tsconfig.json with strict settings and module resolution.

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import { ThemeProvider } from "@/components/layout/theme-provider";
+import type { Metadata } from "next";
+import { Geist, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({ subsets: ["latin"], variable: "--font-heading" });
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SZ Activity",
+  description: "A simple activity tracker for SZ Members",
+  icons: {
+    icon: "/logo.svg",
+    apple: "/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        "font-sans",
+        inter.variable,
+        geistHeading.variable,
+      )}
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">Welcome to SZ Activity</h1>
+    </div>
+  );
+}
